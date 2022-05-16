5ec8223c678588fd4429fb4762e1c5c971d98ffc
Harikrishnan Aji <harikrishnanaji10@gmail.com>
2022-05-16 13:22:39 +0530

    modified

--- src/components/Weather/weather.component.jsx
+++ src/components/Weather/weather.component.jsx
@@ -6,20 +6,20 @@ const Weather = props => {
     <div className="container text-light bg-blue">
       <div className="Card">
         <h1 className="text-white py-2 ">{props.cityname}</h1>
-        <h5 className="py-2">
+        <h5 className="py-1">
           <i className={`wi ${props.weatherIcon} display-1`} />
         </h5>
 
         {/* Get Celsius */}
         {props.temp_celsius ? (
-          <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+          <h1 className="py-1">{props.temp_celsius}&deg;</h1>
         ) : null}
 
         {/* show max and min temp */}
         {maxminTemp(props.temp_min, props.temp_max)}
 
         {/* Weather description */}
-        <h4 className="py-2 ">
+        <h4 className="py-1 ">
           {props.description.charAt(0).toUpperCase() +
             props.description.slice(1)}
         </h4>
@@ -34,8 +34,8 @@ function maxminTemp(min, max) {
   if (max && min) {
     return (
       <h3>
-        <span className="px-4">{min}&deg;</span>
-        <span className="px-4">{max}&deg;</span>
+        <span className="px-1">{min}&deg;</span>
+        <span className="px-1">{max}&deg;</span>
       </h3>
     );
   }
